9851375dd60d5f10ecda2052b3df90ebf8e52a99
alexanderswann-noaa <alexander.swann@noaa.gov>
2024-06-24 17:09:26 -0400

    color fixed and name changes plus other changes

--- batchCleanClassifyFromFolder.py
+++ batchCleanClassifyFromFolder.py
@@ -25,6 +25,7 @@ def octreeLevel(octree):
 
 def clean_classify(path, filename):
     cloud1 = cc.loadPointCloud(path + "/"+filename)
+    cloud1.setName("Original Point Cloud")
 
     res = cloud1.exportCoordToSF(True, True, True)
     sfx = cloud1.getScalarField(3)
@@ -56,7 +57,7 @@ def clean_classify(path, filename):
 
     cloudRef = cc.CloudSamplingTools.sorFilter(knn=6, nSigma=10, cloud=new_cloud)
     (cloud3, res) = new_cloud.partialClone(cloudRef)
-    cloud3.setName("cloud3")
+    cloud3.setName("Cleaned Point Cloud")
 
     # remove-statisitcal-outliers---end
 
@@ -79,24 +80,15 @@ def clean_classify(path, filename):
                                          randomColors=True)
     print(res1)
 
-    # cloud01 = cc.MergeEntities(res1[1], createSFcloudIndex=True)
-    # cloud01.setName("pump_Extract_Components")
-    # cloud02 = cc.MergeEntities(res1[2], createSFcloudIndex=True)
-    # cloud02.setName("pump_residual_Components")
-    # res2 = res1[1] + res1[2] # connected components plus regrouped residual components
-    # cloud2 = cc.MergeEntities(res2, createSFcloudIndex=True)
-    # cloud2.setName("pump_extract_Residual_Components")
-    # cloud3 = cc.MergeEntities(res2, deleteOriginalClouds=True)
-    # cloud3.setName("pump_extract_Residual_Components")
 
     res4 = [res1[1][0]]
     res3 = res1[2] + res4
 
     cloud4 = cc.MergeEntities(res3, createSFcloudIndex=True)
-    cloud4.setName("Rough ground")
+    cloud4.setName("Ground Points: V1")
 
     cloud5 = cc.MergeEntities(res1[1][1:], createSFcloudIndex=True)
-    cloud5.setName("Rough fish " + str(len(res1[1][1:])) + " total: Octree Level " + str(level))
+    cloud5.setName("Fish Points : V1 | " + str(len(res1[1][1:])) + " total fish | Utilized Octree Level for fish segmentation: " + str(level))
 
     print(str(len(res1[1][1:])) + " total fish")
 
@@ -104,18 +96,28 @@ def clean_classify(path, filename):
     # ---extractFish-end
 
     # --- create DEM --- begin
-    cloud4.setCurrentScalarField(5)
+    #cloud4.setCurrentScalarField(5)
     seafloorDEM = cc.RasterizeToMesh(cloud4,
+                                    pathToImages = dataDir,
+                                     outputRasterZ=True,
                                      gridStep=.07,
                                      emptyCellFillStrategy=cc.EmptyCellFillOption.KRIGING,
-                                     projectionType=cc.ProjectionType.PROJ_MINIMUM_VALUE)
+                                     projectionType=cc.ProjectionType.PROJ_MINIMUM_VALUE,
+                                    outputRasterSFs = True)
+    # zz = seafloorDEM.getScalarField(5)
+    # colorScalesManager = cc.ccColorScalesManager.GetUniqueInstance()
+    # scale = colorScalesManager.getDefaultScale(cc.DEFAULT_SCALES.BGYR)
+    # zz.setColorScale(scale)
+    # cc.addToRenderScene(seafloorDEM)
+
+    cc.ccMesh.showSF(seafloorDEM, True)
 
     # --- create DEM --- end
 
     final_cloud = cloud2
     # cloud01, cloud02, cloud2, cloud3,
     # ---export-files-begin
-    res = cc.SaveEntities([cloud3,cloud4, cloud5, seafloorDEM], "CLEAN"+ filename[:-4] + ".bin")
+    res = cc.SaveEntities([cloud1, cloud3,cloud4, cloud5, seafloorDEM], "CLEAN"+ filename[:-4] + ".bin")
 
     # --- export-files-end
 
@@ -127,14 +129,14 @@ print("Files and directories in '", path, "' :")
 print(dir_list)
 
 
-for file in dir_list:
-    if file.endswith(".las"):
-        # Prints only text file present in My Folder
-        print(file)
-        clean_classify(path, file )
+# for file in dir_list:
+#     if file.endswith(".las"):
+#         # Prints only text file present in My Folder
+#         print(file)
+#         clean_classify(path, file )
 
 
-
-#clean_classify("processed_LLS_2024-03-15T051615.010100_0_1.las")
+path = r'C:\Users\Alexander.Swann\PycharmProjects\pythonProject'
+clean_classify(path,"smallsection-processed_LLS_2024-03-15T051615.010100_0_3.las")
 
 
