44d758eab93d89273603135dc2099096c632db0e
alexanderswann-noaa <alexander.swann@noaa.gov>
2024-07-17 16:33:33 -0400

    Update imageInfo.py
    now determining image overlap by time and not min/max x&y

--- src/utils/imageInfo.py
+++ src/utils/imageInfo.py
@@ -9,7 +9,7 @@ from gooey import Gooey, GooeyParser
 import os
 import shutil
 import pandas as pd
-
+from astropy.time import Time
 
 
 
@@ -47,6 +47,22 @@ def _get_if_exist(data, key):
     return None
 
 
+import re
+
+
+def replace_date_colons(date_time_str):
+    # Check if the date is already in the correct format (YYYY-MM-DD)
+    if re.match(r'\d{4}-\d{2}-\d{2}', date_time_str[:10]):
+        return date_time_str
+
+    # If not, replace the colons in the date part with hyphens
+    modified_date_time_str = re.sub(r'(\d{4}):(\d{2}):(\d{2})', r'\1-\2-\3', date_time_str)
+    return modified_date_time_str
+
+
+
+
+
 def _convert_to_degress(value):
     """
     Helper function to convert the GPS coordinates stored in the EXIF to degress in float format
@@ -79,6 +95,8 @@ def get_image_location(filename):
     gps_latitude_ref = _get_if_exist(exif_data, 'GPS GPSLatitudeRef')
     gps_longitude = _get_if_exist(exif_data, 'GPS GPSLongitude')
     gps_longitude_ref = _get_if_exist(exif_data, 'GPS GPSLongitudeRef')
+
+
 
     if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
         lat = _convert_to_degress(gps_latitude)
@@ -128,6 +146,9 @@ def isInside(box, img):
 def is_within_box(image_x, image_y, box):
     return box['min_x'] <= image_x <= box['max_x'] and box['min_y'] <= image_y <= box['max_y']
 
+def is_within_box_Time(image_time, box, secsOffset):
+    return box['Tstart'] - secsOffset <= image_time <= box['Tend'] + secsOffset
+
 
 
 lasinfo = r"C:\Users\Alexander.Swann\.conda\envs\CloudComPy310\pkgs\lastools-20171231-h0e60522_1002\Library\bin\lasinfo.exe"
@@ -136,6 +157,11 @@ lasinfo = r"C:\Users\Alexander.Swann\.conda\envs\CloudComPy310\pkgs\lastools-201
 # las_file = r"C:\Users\Alexander.Swann\Documents\GitHub\Benthic-LiDAR-Mapping\data\processed\project.las"
 # #las_file = r"B:\Xander\Raw Data\Dive013_LAS\laser\processed_LLS_2024-03-15T040816.010100_0_1.las"
 # #las_file = r"B:\Xander\Raw Data\Dive013_LAS\laser\processed_LLS_2024-03-15T042110.010100_1_1.las"
+
+las_file = r"Z:\NCCOS-temp\Swann\data\processed\all14processed\lasFiles\SMALLprocessed_LLS_2024-03-15T052913.010100_0_3.las"
+las_file = r"B:\Xander\outputs\dive07\images\bad\BADprocessed_LLS_2024-06-29T041621.007600_1_2.las\BADprocessed_LLS_2024-06-29T041621.007600_1_2.las"
+
+las_file = r"Z:\NCCOS-temp\Swann\data\output\SMALLprocessed_LLS_2024-03-15T052240.010100_0_2.las\SMALLprocessed_LLS_2024-03-15T052240.010100_0_2.las"
 #
 #
 # #filename = r"Z:\NCCOS-temp\Swann\OR2401_DIVE014\Images\ESC_stills_processed_2024-03-15T060242.523586_12443.jpg"
@@ -144,12 +170,107 @@ lasinfo = r"C:\Users\Alexander.Swann\.conda\envs\CloudComPy310\pkgs\lastools-201
 #
 #
 #
+
+filename = r"Z:\NCCOS-temp\Swann\data\output\SMALLprocessed_LLS_2024-03-15T052240.010100_0_2.las\ESC_stills_processed_2024-03-15T052337.523586_8034.jpg"
 # imgUTM = getImgUTM(filename)
 # print(imgUTM)
 #
 # #____
-# lasinfo_report = getLasInfo(lasinfo, las_file)
+exif_data = get_image_exif(filename)
+
+
+def getImgTIME(filename):
+    exif_data = get_image_exif(filename)
+
+    dateOriginal = str(_get_if_exist(exif_data, 'EXIF DateTimeOriginal'))
+    secOriginal = str(_get_if_exist(exif_data, 'EXIF SubSecTimeOriginal'))
+
+    date_sec_og = dateOriginal + secOriginal[1:]
+
+    imgTime = replace_date_colons(date_sec_og)
+
+    img_t_in = Time(imgTime, format='iso', scale='utc')
+    img_t_out = Time(img_t_in, format='unix')
+
+
+    return img_t_out.to_value('unix', subfmt='float')
+
+
+dateOriginal = str(_get_if_exist(exif_data, 'EXIF DateTimeOriginal'))
+secOriginal = str(_get_if_exist(exif_data, 'EXIF SubSecTimeOriginal'))
+
+print(dateOriginal)
+print(secOriginal)
+
+print(dateOriginal + secOriginal[1:])
+
+date_sec_og = dateOriginal + secOriginal[1:]
+
+imgTime = replace_date_colons(date_sec_og)
+
+
+date_time_str1 = "2024:03:15 05:23:38"
+
+print(replace_date_colons(date_time_str1))  # Output: 2024-03-15 05:23:38
+
+def TIMEparse_lasinfo_report(report):
+    min_pattern = re.compile(r'gps_time\s+([\d\.]+)\s+([\d\.]+)')
+    min_match = min_pattern.search(report)
+    min_values = list(map(float, min_match.groups()))
+
+    startTime = min_values[0] + 1000000000
+    endTime = min_values[1] + 1000000000
+
+    sec = [startTime, endTime]
+    las_t_in = Time(sec, format='gps')
+    las_t_out = Time(las_t_in, format='iso', scale='utc')
+    print(las_t_out)
+
+    las_t_out2 = Time(las_t_out, format='unix')
+
+
+    return las_t_out2.to_value('unix', subfmt='float')
+
+
+
+
+lasinfo_report = getLasInfo(lasinfo, las_file)
+min_pattern = re.compile(r'gps_time\s+([\d\.]+)\s+([\d\.]+)')
+min_match = min_pattern.search(lasinfo_report)
+min_values = list(map(float, min_match.groups()))
+
+startTime = min_values[0] + 1000000000
+endTime = min_values[1] + 1000000000
+
+
+sec = [startTime, endTime]
+las_t_in = Time(sec, format='gps')
+las_t_out = Time(las_t_in, format='iso', scale='utc')
+print(las_t_out)
+
+
+las_t_out2 = Time(las_t_out, format='unix').to_value('unix', subfmt='float')
+
+
+print("Time 1")
+print(las_t_out2[0], las_t_out2[1])
+
+
+img_t_in = Time(imgTime, format = 'iso', scale='utc')
+img_t_out = Time(img_t_in, format = 'unix').to_value('unix', subfmt='float')
+print("Time 2")
+
+print(img_t_out)
+
+
+print(min_values)
+
+secsOffset = 4
+result = las_t_out2[0] - secsOffset <= img_t_out <= las_t_out2[1] + secsOffset
+print(result)
 #
+
+# print(exif_data)
 # print(lasinfo_report)
 #
 # min_vals, max_vals = parse_lasinfo_report(lasinfo_report)
@@ -209,21 +330,21 @@ def main():
 
             lasinfo_report = getLasInfo(lasinfo, (os.path.join(directory, file)))
 
-            print(lasinfo_report)
+            #print(lasinfo_report)
 
 
-            min_vals, max_vals = parse_lasinfo_report(lasinfo_report)
-            print(min_vals)
+            min_vals = TIMEparse_lasinfo_report(lasinfo_report)
+            print(min_vals[0], min_vals[1])
 
             # #new_row = pd.DataFrame(
             #     {'min_x': [min_vals[0]], 'min_y': [min_vals[1]], 'max_x': [max_vals[0]], 'max_y': [max_vals[1]],
             #      'box_file_name': [file], 'box_file_path': [(os.path.join(directory, file))]})
-            tracksData.append([ min_vals[0], min_vals[1],  max_vals[0], max_vals[1], file, (os.path.join(directory, file))   ])
+            tracksData.append([ min_vals[0], min_vals[1], file, (os.path.join(directory, file))   ])
             #print(new_row)
 
             #pd.concat([tracksDf, new_row], ignore_index=False)
 
-    tracksDf = pd.DataFrame(tracksData, columns=['min_x', 'min_y', 'max_x', 'max_y', 'box_file_name', 'box_file_path'])
+    tracksDf = pd.DataFrame(tracksData, columns=['Tstart', 'Tend', 'box_file_name', 'box_file_path'])
     output_base_dir = outputDirectory
     os.makedirs(output_base_dir, exist_ok=True)
 
@@ -237,16 +358,16 @@ def main():
                 print("Processing file:", file)
 
 
-                vals = getImgUTM((os.path.join(imgdirectory, file)))
+                valTime = getImgTIME((os.path.join(imgdirectory, file)))
 
-                imagesData.append([ vals[0], vals[1], (os.path.join(imgdirectory, file)), file ])
+                imagesData.append([ valTime, (os.path.join(imgdirectory, file)), file ])
 
                 #new_row = pd.DataFrame(
                     # {'x': [vals[0]], 'y': [vals[1]],
                     #   'file_path': [(os.path.join(imgdirectory, file))], 'file_name': [file]})
 
                 #pd.concat([imagesDf, new_row], ignore_index=False)
-        imagesDf = pd.DataFrame(imagesData, columns=['x', 'y', 'file_path', 'file_name'])
+        imagesDf = pd.DataFrame(imagesData, columns=['time', 'file_path', 'file_name'])
 
         imagesDf.to_csv(os.path.join(output_base_dir, "images.csv"))
     else:
@@ -288,7 +409,9 @@ def main():
         box_file_name = box['box_file_name']
         box_dir = os.path.join(output_base_dir, box_file_name)
 
-        images_in_box = images_df[images_df.apply(lambda img: is_within_box(img['x'], img['y'], box), axis=1)]
+        secsOffset = 0
+
+        images_in_box = images_df[images_df.apply(lambda img: is_within_box_Time(img['time'], box, secsOffset), axis=1)]
 
         for _, image in images_in_box.iterrows():
             shutil.copy(image['file_path'], os.path.join(box_dir, image['file_name']))
