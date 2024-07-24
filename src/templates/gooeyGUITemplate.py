c616f1f15e698559c158e8327d8ad9d5666fbda1
alexanderswann-noaa <alexander.swann@noaa.gov>
2024-07-24 14:51:48 -0400

    new templates
    new templates will be using this structure moving forward
    
    Co-Authored-By: alexanderswann <32579236+alexanderswann@users.noreply.github.com>

---
+++ src/templates/gooeyGUITemplate.py
@@ -0,0 +1,80 @@
+# -----------------------------------------------------------------------------------------------------------
+# Import Funtions
+# -----------------------------------------------------------------------------------------------------------
+
+import time
+import traceback
+import argparse
+
+import numpy as np
+from gooey import Gooey, GooeyParser
+
+from classTemplate import myClass as mc
+
+
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Gooey GUI
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+
+@Gooey
+def main():
+    """
+    Main function to parse arguments and initiate processing.
+    """
+    desc = 'Process and classify point clouds.'
+    parser = GooeyParser(description=desc)
+
+
+    parser.add_argument('--verbose', help='be verbose', dest='verbose',
+                        action='store_true', default=False)
+
+    subs = parser.add_subparsers(help='commands', dest='command')
+
+#____
+    clean_parser = subs.add_parser('clean')
+    group1 = clean_parser.add_argument_group('General')
+    group1.add_argument('path', type=str, default = "default1",help='Directory containing the LAS files.')
+    group1.add_argument('--output_dir', type=str, default='default1', help='Directory to save the processed files.')
+    group1.add_argument('--file', type=str, default='default1', help='Directory to save the processed files.')
+
+
+
+
+    process_parser = subs.add_parser('process')
+    group2 = process_parser.add_argument_group('processing')
+    group2.add_argument('path', type=str, default = "default2", help='Directory containing the LAS files.')
+    group2.add_argument('--output_dir', type=str, default='default2', help='Directory to save the processed files.')
+    group2.add_argument('--file', type=str, default='default2', help='Directory to save the processed files.')
+
+
+
+
+
+
+    args = parser.parse_args()
+
+
+    workflow = mc.fromArgs(args=args)
+
+    workflow.run()
+
+
+
+    if args.command == 'clean':
+        print("clean")
+
+    if args.command == 'process':
+        print("process")
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
